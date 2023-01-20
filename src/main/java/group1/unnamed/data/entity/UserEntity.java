eaac42eb1a820eb7e19ca1e744daa731721e366a
ssangjun <uio3001@dongguk.edu>
2023-01-20 20:36:33 +0900

    feat: 유저 엔티티 추가

---
+++ src/main/java/group1/unnamed/data/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package group1.unnamed.data.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "id")
+    private CompanyEntity companyId;
+    private String code;
+    private String name;
+    private String email;
+    private String password;
+
+}
